c3cefa978a547098b934c500ce818b556c2822f8
robisys <robert@robisys.de>
2016-02-18 13:56:05 +0100

    Update myprog.rs

--- src/myprog.rs
+++ src/myprog.rs
@@ -1,6 +1,16 @@
 
 //
+#[cfg(unix)]
+fn is_executable() {
+   println!("is unix");
+   }
+#[cfg(windows)]
+fn is_executable() {
+   println!("is windows");
+   }   
+
 fn main() {
+is_executable();
 println!("Hallo World");
 }
 #[test]
