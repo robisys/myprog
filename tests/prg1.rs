45fc9f466272dd940a395a6a9ade5f2cda7cfb7a
Robert P <robisys@users.noreply.github.com>
2016-02-22 11:33:13 +0100

    Create prg1.rs

---
+++ tests/prg1.rs
@@ -0,0 +1,14 @@
+#[test]
+fn parameterised_path_with_format() {
+  //  with_path("/not_bar.xml", |res| {
+  //      let s = read_body_to_string(res);
+  //      assert_eq!(s, "Foo is 'not_bar'. The requested format is 'xml'");
+  //  })
+}
+
+#[test]
+fn fallthrough_with_no_match() {
+//with_path("/foo/bar", |res| {
+//        assert_eq!(res.status, StatusCode::NotFound);
+    })
+}
