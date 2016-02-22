4116ef2866366b9487b68bad0fc740fe5e9c04e2
Robert P <robisys@users.noreply.github.com>
2016-02-22 14:06:39 +0100

    Added files via upload

---
+++ tests/example_tests.rs
@@ -0,0 +1,22 @@
+extern crate rustc_serialize;
+extern crate hyper;
+#[macro_use] extern crate lazy_static;
+
+mod util;
+
+mod examples {
+    mod hello_world;
+/*
+    mod mount;
+    mod route_data;
+    mod routing;
+    mod template;
+    mod moved_ownership;
+    mod chaining;
+    mod json;
+    mod query_string;
+    mod regex_route;
+    mod custom_error_handler;
+    mod static_files;
+  */
+}
